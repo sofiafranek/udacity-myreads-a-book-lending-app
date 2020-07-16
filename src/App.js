a22c53475d9b52ade5d1b959b9d628ae8382f34d
Sofia Franek <sofiafranek@icloud.com>
2020-07-16 11:53:58 +0100

    Taking break.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI';
+import * as BooksAPI from './utils/BooksAPI';
 import ListBooks from './views/ListBooks';
 import SearchBooks from './views/SearchBooks';
 
--- src/views/SearchBooks.js
+++ src/views/SearchBooks.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Book from '../components/Book';
-import * as BooksAPI from '../BooksAPI';
+import * as BooksAPI from '../utils/BooksAPI';
 import { debounce } from 'lodash';
 
 class SearchBooks extends Component {
